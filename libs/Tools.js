c2eef65d0b5d1f41329da54571e46d12fb380a7e
Kcars <kcars.log@gmail.com>
2022-10-21 15:00:17 +0800

    add gfit event and something else

--- libs/Tools.js
+++ libs/Tools.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 
 const setting = require("../setting.json");
+const stopwords = require("../stopword.json");
 const channel = require("../Channel");
 
 const axios = require('axios');
@@ -15,25 +16,31 @@ client.connect();
 const url_stat = "https://www.googleapis.com/youtube/v3/videos";
 const part = "id,snippet,statistics,liveStreamingDetails";
 
-const doAppendRecord = (yyyy, mm, videoId, text) => {
-    let path = `./stats/records-${yyyy}${mm < 10 ? `0${mm}` : mm}.ndjson`;
+const doAppendRecord = async (yyyy, mm, channelId, videoId, text) => {
+    let path = `./stats/records-${yyyy}${mm}.ndjson`;
 
-    client.DEL(`${prefix}:live:${videoId}`);
-    client.DEL(`${prefix}:message_ids:${videoId}`);
+    await client.DEL(`${prefix}:live:${videoId}`);
+    await client.DEL(`${prefix}:message_ids:${videoId}`);
 
-    client.SREM(`${prefix}:live:ids`, videoId);
-    client.SREM(`${prefix}:upcoming:ids`, videoId);
+    await client.SREM(`${prefix}:live:ids`, videoId);
+    await client.SREM(`${prefix}:upcoming:ids`, videoId);
 
-    client.SADD(`${prefix}:old:ids`, videoId);
+    await client.SADD(`${prefix}:old:ids`, videoId);
 
-    fs.readFile(path, (err, output) => {
+    fs.readFile(path, async (err, output) => {
         let content = output != null && output != "" ? output.toString("utf8") : "";
         let not_append = content.indexOf(`{"id":"${videoId}"`) == -1 || content == "";
 
         if (not_append) {
-            fs.appendFile(path, text, (err, res) => {
+            fs.appendFile(path, text, async (err, res) => {
+                let lang = channel.getLang(channelId);
+                let content = `${videoId} ${yyyy} ${mm} ${lang}`;
+
                 console.log(`[INFO][${yyyy}/${mm}][${videoId}] append to records. ${path}`);
-                client.RPUSH(`${prefix}:record:queue`, `${videoId} ${yyyy} ${mm}`);
+                console.log(`[DEBUG] content: ${content}`);
+
+                // 用bash script每分鐘處理
+                await client.RPUSH(`${prefix}:record:queue`, content);
             });
         }
     });
@@ -71,17 +78,21 @@ const parseVideo = async (video) => {
 
             if (Object.keys(item).length > 0) {
                 let item_str = `${JSON.stringify(item)}\r\n`;
-                doAppendRecord(yyyy, mm, videoId, item_str);
+
+                mm = mm < 10 ? `0${mm}` : mm;
+
+                await doAppendRecord(yyyy, mm, channelId, videoId, item_str);
             } else {
+                await client.SADD(`${prefix}:old:ids`, videoId);
                 //console.log(`${key} is empty`);
             }
         } else {
-            let isLive = false;
-
             // 先全部移除
             await client.SREM(`${prefix}:live:ids`, videoId);
             await client.SREM(`${prefix}:upcoming:ids`, videoId);
             await client.SREM(`${prefix}:old:ids`, videoId);
+
+            let playTime = '00:00:00';
 
             // 重新加入
             if (videoStatus == 'upcoming') {
@@ -90,12 +101,12 @@ const parseVideo = async (video) => {
 
             if (videoStatus == 'live') {
                 await client.SADD(`${prefix}:live:ids`, videoId);
-                isLive = true
-            }
 
-            let startTimeSec = isLive ? new Date(startTime).getTime() : new Date().getTime();
-            let playTimeSec = (new Date().getTime() - startTimeSec) / 1000;
-            let playTime = isLive ? new Date(playTimeSec * 1000).toISOString().substring(11, 19) : '00:00:00';
+                let startTimeSec = videoStatus == 'live' ? new Date(startTime).getTime() : new Date().getTime();
+                let playTimeSec = (new Date().getTime() - startTimeSec) / 1000;
+
+                playTime = new Date(playTimeSec * 1000).toISOString().substring(11, 19)
+            }
 
             // 更新資訊
             await client.HSET(`${key}`, 'status', videoStatus);
@@ -161,6 +172,9 @@ const incrVideoHash = async (videoId, propName, val) => {
     return true;
 }
 
+/**
+ * 產生目前的直播資訊資訊物件
+ */
 const getVideoJson = async () => {
     let dd = new Date();
 
@@ -180,6 +194,9 @@ const getVideoJson = async () => {
     return output;
 }
 
+/**
+ * 將目前的直播資訊寫成檔案
+ */
 const writeVideoJson = async () => {
     let path = 'stats/video.json';
     let json = await getVideoJson();
@@ -190,6 +207,12 @@ const writeVideoJson = async () => {
     });
 }
 
+/**
+ * 將SC的單位轉成日幣
+ * @param {Number} amount_micros 
+ * @param {String} currency 
+ * @returns 
+ */
 const getRealAmount = (amount_micros, currency) => {
     switch (currency) {
         case "GBP": // 1GBP=?JPY
@@ -247,14 +270,22 @@ const getRealAmount = (amount_micros, currency) => {
     return amount_micros;
 }
 
+/**
+ * 解析聊天訊息並整理出需要的物件
+ * @param {Object} video 
+ * @param {Object} item 
+ * @returns 
+ */
 const parseLiveChatMessage = async (video, item) => {
     return new Promise(async (resolve, reject) => {
         let path = `chats/${video.id}.ndjson`;
-        let mkey = `${prefix}:message_ids:${video.id}`;
+        let mkey = `${prefix}:message_ids:${video.id}`; // 其實不必要
 
         let isAdd = await client.SISMEMBER(mkey, item.id);
 
         if (!isAdd) {
+            let type = item.snippet.type;
+
             let startTimeSec = new Date(video.live_start_time).getTime();
             let playTimeSec = (new Date(item.snippet.publishedAt).getTime() - startTimeSec) / 1000;
             let playTime = new Date(playTimeSec * 1000).toISOString().substring(11, 19);
@@ -264,7 +295,11 @@ const parseLiveChatMessage = async (video, item) => {
             let amount_micros = 0;
             let currency = "none";
 
-            let is_new_sponsor = item.snippet.type == "newSponsorEvent";
+            let is_new_sponsor = type == "newSponsorEvent";
+
+            if (playTimeSec <= 0 || video.live_start_time == 0) {
+                playTime = '00:00:00';
+            }
 
             amount = item.snippet.superChatDetails != null ? item.snippet.superChatDetails.amountDisplayString : amount;
             amount_micros = item.snippet.superChatDetails != null ? item.snippet.superChatDetails.amountMicros : amount_micros;
@@ -274,10 +309,18 @@ const parseLiveChatMessage = async (video, item) => {
             amount_micros = item.snippet.superStickerDetails != null ? item.snippet.superStickerDetails.amountMicros : amount_micros;
             currency = item.snippet.superStickerDetails != null ? item.snippet.superStickerDetails.currency : currency;
 
-            content = item.snippet.type == "newSponsorEvent" ? item.snippet.displayMessage : content;
-            content = item.snippet.type == "textMessageEvent" ? item.snippet.textMessageDetails.messageText : content;
-            content = item.snippet.type == "superChatEvent" ? item.snippet.superChatDetails.userComment : content;
-            content = item.snippet.type == "superStickerEvent" ? item.snippet.superStickerDetails.superStickerMetadata.altText : content;
+            content = type == "newSponsorEvent" ? item.snippet.displayMessage : content;
+            content = type == "textMessageEvent" ? item.snippet.textMessageDetails.messageText : content;
+            content = type == "superChatEvent" ? item.snippet.superChatDetails.userComment : content;
+            content = type == "superStickerEvent" ? item.snippet.superStickerDetails.superStickerMetadata.altText : content;
+
+            /*if (!['newSponsorEvent', 'textMessageEvent', 'superChatEvent', 'superStickerEvent'].includes(item.snippet.type)) {
+                let log_str = JSON.stringify(item);
+
+                fs.appendFile('./livechat.log', `${log_str}\r\n`, (err, res) => {
+
+                });
+            }*/
 
             amount_micros = getRealAmount(amount_micros, currency); // 匯率一直沒有更新
 
@@ -287,7 +330,7 @@ const parseLiveChatMessage = async (video, item) => {
                 channel_id: video.channel_id,
                 video_id: video.id,
                 chat_id: video.chat_id,
-                user_id: item.snippet.authorChannelId,
+                user_id: item.authorDetails.channelId,
                 user_name: item.authorDetails.displayName,
                 is_verified: item.authorDetails.isVerified,
                 is_owner: item.authorDetails.isChatOwner,
@@ -320,9 +363,9 @@ const parseLiveChatMessage = async (video, item) => {
             });
 
             if (inChannelList) { // 如果是在頻道清單內的人
-                let user_icon = item.authorDetails.profileImageUrl;
+                let url_icon = item.authorDetails.profileImageUrl;
                 let json = {
-                    user_icon,
+                    url_icon,
                     content: output.content,
                     video_id: output.video_id,
                     play_time: output.play_time,
@@ -333,9 +376,63 @@ const parseLiveChatMessage = async (video, item) => {
                     amount_micros: output.amount_micros
                 }
 
-                console.log(json);
+                let hasStopFirstword = stopwords.startWords.some((val) => {
+                    return output.content.indexOf(val) == 0
+                })
 
-                // doSendLiveChatCard(json)
+                let hasStopKeyword = stopwords.keywords.some((val) => {
+                    return output.content.indexOf(val) != -1
+                })
+
+                if (hasStopFirstword || hasStopKeyword) {
+                    console.log('include stopwords. skip it.');
+                } else {
+                    doSendLiveChatCard(json);
+                }
+
+                let yyyy = new Date().getFullYear();
+                let mm = new Date().getMonth() + 1;
+
+                mm = mm < 10 ? `0${mm}` : mm;
+
+                let liver_message_path = `chats/liver-${yyyy}${mm}.ndjson`;
+                fs.appendFile(liver_message_path, `${output_str}\r\n`, (err, res) => {
+
+                });
+            }
+
+            if (type == 'membershipGiftingEvent') {
+                let output = {};
+
+                let yyyy = new Date().getFullYear();
+                let mm = new Date().getMonth() + 1;
+
+                mm = mm < 10 ? `0${mm}` : mm;
+                
+                try {
+                    output = {
+                        to_channel_id: video.channel_id,
+                        to_channel_name: video.channel_title,
+                        from_channel_id: item.authorDetails.channelId,
+                        from_channel_name: item.authorDetails.displayName,
+                        video_id: video.id,
+                        video_name: video.title,
+                        gift_count: item.snippet.membershipGiftingDetails.giftMembershipsCount,
+                        gift_level: item.snippet.membershipGiftingDetails.giftMembershipsLevelName,
+                        post_time: item.snippet.publishedAt,
+                        play_time: playTime,
+                    };
+                } catch (err) {
+                    console.log('gifting output json failed.');
+                }
+
+                let output_str = JSON.stringify(output);
+
+                fs.appendFile(`./gifting-${yyyy}${mm}.log`, `${output_str}\r\n`, (err, res) => {
+
+                });
+
+                doSendGiftingMessage(output);
             }
 
             resolve({ is_new_sponsor, amount_micros });
@@ -343,14 +440,34 @@ const parseLiveChatMessage = async (video, item) => {
     })
 }
 
+/**
+ * 轉送贈送聊天訊息
+ * @param {Object} json 
+ */
 const doSendLiveChatCard = (json) => {
     console.log(`post 127.0.0.1:3456: `);
-    console.log(json);
-    axios.post('http://127.0.0.1:3456', json);
+    console.log(JSON.stringify(json));
+    axios.post('http://127.0.0.1:3456', json).catch((err) => {
+        console.log('doSendLiveChatCard failed')
+    });
 }
 
-const getChannelInfoList = (id) => { // id= id1,id2,id3 ...
-    console.log('getChannelInfoList')
+/**
+ * 轉送贈送會員資訊
+ * @param {Object} json 
+ */
+const doSendGiftingMessage = (json) => {
+    axios.post('http://127.0.0.1:3456/gifting', json).catch((err) => {
+        console.log('doSendGiftingMessage failed')
+    });
+}
+
+/**
+ * 取得指定頻道編號的頻道資訊
+ * @param {String} id 
+ * @returns 
+ */
+const getChannelInfoList = (id) => {
     return new Promise((resolve, reject) => {
         let url_stat = "https://www.googleapis.com/youtube/v3/channels";
         let key = setting.youtube.key;
@@ -371,6 +488,11 @@ const getChannelInfoList = (id) => { // id= id1,id2,id3 ...
     })
 }
 
+/**
+ * 取得指定影片的聊天室訊息
+ * @param {Object} video 
+ * @returns 
+ */
 const getLiveChat = (video) => {
     return new Promise((resolve, reject) => {
         let url_chat = "https://www.googleapis.com/youtube/v3/liveChat/messages";
@@ -433,17 +555,25 @@ const getLiveChat = (video) => {
                 reject(-1);
             }
 
-            client.SREM(`${prefix}:live:ids`, video.id);
-            client.SREM(`${prefix}:upcoming:ids`, video.id);
+            // 在doAppendRecord一併處理 偶爾會重複出錯也無所謂
 
-            client.DEL(`${prefix}:live:${video.id}`);
-            client.DEL(`${prefix}:message_ids:${video.id}`);
+            //client.SREM(`${prefix}:live:ids`, video.id);
+            //client.SREM(`${prefix}:upcoming:ids`, video.id);
+
+            //client.DEL(`${prefix}:live:${video.id}`);
+            //client.DEL(`${prefix}:message_ids:${video.id}`);
 
             resolve({ result: false, errReason })
         });
     })
 }
 
+/**
+ * 將傳入陣列分割成指定最大數量的陣列集合
+ * @param {Array} arr 
+ * @param {Int} size 
+ * @returns [ [] , [] ... ]
+ */
 exports.getChunkArray = (arr, size = 40) => {
     let index = 0;
     let arrLen = arr.length;
@@ -457,6 +587,10 @@ exports.getChunkArray = (arr, size = 40) => {
     return output;
 }
 
+/**
+ * 取得直播待播的影片編號
+ * @returns Array
+ */
 exports.getCurrentNotRecordVideoIds = async () => {
     let liveIds = await client.SMEMBERS(`${prefix}:live:ids`);
     let upcomingIds = await client.SMEMBERS(`${prefix}:upcoming:ids`);
@@ -465,6 +599,11 @@ exports.getCurrentNotRecordVideoIds = async () => {
     return output;
 }
 
+/**
+ * 過濾掉非直播待播的影片編號
+ * @param {Array} videoIds 
+ * @returns 
+ */
 exports.getNotRecordVideoIds = (videoIds) => {
     return new Promise(async (resolve) => {
         let output = [];
@@ -482,6 +621,11 @@ exports.getNotRecordVideoIds = (videoIds) => {
     })
 }
 
+/**
+ * 取得指定影片編號的資訊
+ * @param {Array} videoIds [videoid1, videoid2 ...]
+ * @returns 
+ */
 exports.parseVideoByIds = async (videoIds) => {
     return new Promise(async (resolve, reject) => {
         let key = setting.youtube.key;
@@ -500,7 +644,7 @@ exports.parseVideoByIds = async (videoIds) => {
                 }
 
                 // 回傳資料有漏=有影片取得不到資訊=移除它
-                // 有可能是先暫時轉私人 所以不要放到old裡
+                // 有可能是先暫時轉私人
                 if (videos.length != videoIds.length) {
                     let backVideoIds = videos.map((video) => video.id);
 
@@ -508,8 +652,13 @@ exports.parseVideoByIds = async (videoIds) => {
                         let inBackList = backVideoIds.includes(videoId);
 
                         if (!inBackList && videoId != null && videoId != '') {
+                            console.log(`[WARN] ${videoId} can not get info.`);
+
                             await client.SREM(`${prefix}:live:ids`, videoId);
                             await client.SREM(`${prefix}:upcoming:ids`, videoId);
+
+                            await client.DEL(`${prefix}:live:${videoId}`);
+                            await client.DEL(`${prefix}:message_ids:${videoId}`);
                         }
                     }
                 }
@@ -519,21 +668,6 @@ exports.parseVideoByIds = async (videoIds) => {
                 reject(-2);
             })
         }
-
-        // debug
-        /*
-            let liveIds = await client.SMEMBERS(`${prefix}:live:ids`);
-            let upcomingIds = await client.SMEMBERS(`${prefix}:upcoming:ids`);
-            let oldIds = await client.SMEMBERS(`${prefix}:old:ids`);
-            
-            let liveKeys = await client.KEYS(`${prefix}:live:*`) ;
-        
-            console.log(`live: ${liveIds.join(" , ")}`);
-            console.log(`upcoming: ${upcomingIds.join(" , ")}`);
-            console.log(`old: ${oldIds.join(" , ")}`);
-        
-            console.log(`live:\r\n${liveKeys.join("\r\n")}`);
-        */
     })
 }
 
